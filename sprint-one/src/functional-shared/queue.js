749e6b42db4698dabedbb4cf4480718129cda395
iam-peekay <iam.preethi.k@gmail.com>
2015-10-07 14:55:01 -0700

    Implement Queue function using functional shared - all tests pass

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.front = 0;
+  queue.back = 0;
+  _.extend(queue, queueMethods);
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  },
+
+  dequeue: function() {
+    if (this.size()) {
+      var result = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front++;
+      return result;
+    }
+  }, 
+
+  size: function() {
+    return this.back - this.front;
+  }
+
+};
 
 
