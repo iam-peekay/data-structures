5ed1cd6fe9de3ccab7e9da0bc86d925d3b5aad3a
iam-peekay <iam.preethi.k@gmail.com>
2015-10-07 15:25:51 -0700

    Pseudoclassical stack passes all tests.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+};
 
+Stack.prototype.pop = function() {
+  if (this.length > 0) {
+    var result = this.storage[this.length -1];
+    delete this.storage[this.length];
+    this.length--;
+    return result;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
