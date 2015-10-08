429e1006eac05494687d862dca01b9d358a7e4d5
iam-peekay <iam.preethi.k@gmail.com>
2015-10-08 15:16:27 -0700

    Implement set using prototypal - all tests pass.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if (this._storage.indexOf(item) === -1) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.indexOf(item) > -1;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  this._storage.splice(index, 1);
 };
 
 /*
