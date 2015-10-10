a2210044f82fe95d4701269f5b22bb06825d1292
iam-peekay <iam.preethi.k@gmail.com>
2015-10-10 15:34:45 -0700

    Implement graph - refactored. All tests pass.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,133 +3,93 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
-  this.nodeList = [];
+  // object: key is node's value, value is an array of edges it's connected to
+  this._nodes = {};
 };
 
-var newNode = function(value) {
-  this.edges = [];
-  this.value = value;
-}
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  var node = new newNode(node);
-  this.nodeList.push(node);
-};
+  // check if node exists,
+    // if yes, don't do anything
+  // else, set to a nested empty object with edges
+  this._nodes[node] = this._nodes[node] || { edges: {} };
+ };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for (var i = 0; i < this.nodeList.length; i++) {
-    if (this.nodeList[i].value === node) {
-      return true;
-    }
-  }
-  return false;
+  return !!this._nodes[node];
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  // loop through node list to find current node to remove, remove it
-  // hold the value of the removed node in temp
-  // loop through the updated node list and for each node, check if it "hasEdge" with deleted node
-    // if yes, removeEdge
-  var temp = node;
-  for (var i = 0; i < this.nodeList.length; i++) {
-    if (this.nodeList[i].value === node) {
-      this.nodeList.splice(i, 1);
-    }
+  // check if node exists
+  if (!this.contains(node)) {
+    // if not, return
+    return;
   }
-
-  for (var i = 0; i < this.nodeList.length; i++) {
-    if (this.hasEdge(this.nodeList[i].value, temp)) {
-      this.removeEdge(this.nodeList[i].value, temp);
-    }
-   }
+  // if it does
+  if (this.contains(node)) {
+    // loop through the current node's edges
+      // remove edges between node to be deleted and all other connected nodes
+      for (var targetNode in this._nodes[node].edges) {
+        this.removeEdge(node, targetNode);
+      }
+    // delete the node
+    delete this._nodes[node];
+  }
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  var containsFrom = false;
-  var fromLoc;
-  var containsTo = false;
-  var toLoc;
-  for (var i = 0; i < this.nodeList.length; i++) {
-    if (this.nodeList[i].value === fromNode) {
-      containsFrom = true;
-      fromLoc = this.nodeList[i].edges;
-    }
-    if (this.nodeList[i].value === toNode) {
-      containsTo = true;
-      toLoc = this.nodeList[i].edges;
-    }
-  }
-    // debugger;
-  if (fromLoc.indexOf(toNode) > -1 && toLoc.indexOf(fromNode) > -1) {
-
-    return true;
-  } else {
+  // if there is no fromNode OR there is no toNode
+  if (!this.contains(fromNode)) {
+    // then return
     return false;
   }
+  // look at the edges of the fromNode and check if the toNode exists
+  return !!this._nodes[fromNode].edges[toNode];
+    // if yes, return true
+
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  var containsFrom = false;
-  var fromLoc;
-  var containsTo = false;
-  var toLoc;
-  for (var i = 0; i < this.nodeList.length; i++) {
-    if (this.nodeList[i].value === fromNode) {
-      containsFrom = true;
-      fromLoc = this.nodeList[i].edges;
-    }
-    if (this.nodeList[i].value === toNode) {
-      containsTo = true;
-      toLoc = this.nodeList[i].edges;
-    }
+  // if there is no fromNode OR there is no toNode
+  if (!this.contains(fromNode) || !this.contains(toNode)) {
+    // then return
+    return;
   }
 
-  if (containsFrom && containsTo) {
-    fromLoc.push(toNode);
-    toLoc.push(fromNode);
-  }
-
+  this._nodes[fromNode].edges[toNode] = toNode;
+  this._nodes[toNode].edges[fromNode] = fromNode;
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  var containsFrom = false;
-  var fromLoc;
-  var containsTo = false;
-  var toLoc;
-  for (var i = 0; i < this.nodeList.length; i++) {
-    if (this.nodeList[i].value === fromNode) {
-      containsFrom = true;
-      fromLoc = this.nodeList[i].edges;
-    }
-    if (this.nodeList[i].value === toNode) {
-      containsTo = true;
-      toLoc = this.nodeList[i].edges;
-    }
+  // if there is no fromNode OR there is no toNode
+  if (!this.contains(fromNode) || !this.contains(toNode)) {
+    // then return
+    return;
   }
-  
-  if (containsFrom && containsTo) {
-    fromLoc.splice(fromLoc.indexOf(toNode), 1);
-    toLoc.splice(toLoc.indexOf(fromNode), 1);
-  }
+
+  delete this._nodes[fromNode].edges[toNode];
+  delete this._nodes[toNode].edges[fromNode];  
 
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  _.each(this.nodeList, function(node) {
-    cb(node.value);
+  // loop through each key in the graph
+    // execute callback function on the key 
+  _.each(this._nodes, function(value, key) {
+    cb(key);
   });
 };
 
