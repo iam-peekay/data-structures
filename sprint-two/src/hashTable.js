aa9462e8a66ec8bc5e64ec7166b1e408535fca45
iam-peekay <iam.preethi.k@gmail.com>
2015-10-10 16:05:24 -0700

    Refactor hash table - all tests pass.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,102 +7,104 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  // get hashed index
   var index = getIndexBelowMaxForKey(k, this._limit);
-
-  // bucket is the tuple at the current index
-  var bucket = this._storage.get(index);
-  // if bucket, then check if the current key matches any keys in the bucket
-  if (bucket) {
-    // loop through bucket's arrays
-    for (var i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) {
-        bucket[i] = [k, v];
-        break;
-      }
+  // bucket is the array of tuples at the current index or empty array if index doesn't exist
+  var bucket = this._storage.get(index) || [];
+  // loop through the bucket
+  for (var i = 0; i < bucket.length; i++) {
+  // check if the current tuple key matches the new key to insert
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      // if it does, overwrite the value for the key with the new value
+        // exit
+      tuple[1] = v;
+      return;
     }
-    bucket.push([k, v]);
-  } else {
-    this._storage.set(index, [[k, v]]);
   }
 
+    // else, push the current key and value as a tuple into the bucket 
+  bucket.push([k, v]);
+    // set the current index to the new updated bucket
+  this._storage.set(index, bucket);
+    // increment size of hash table
   this._length++;
-  if (this._limit * 0.75 < this._length) {
-    this.resize();
+    // check if hash table need to be resized
+  if (this._length > this._limit * 0.75) {
+      // if yes. run the resizer function
+    this._resize(this._limit * 2);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
+  // get hashed index
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
+  // bucket is the array of tuples at the current index or empty array if index doesn't exist
+  var bucket = this._storage.get(index) || [];
+  // loop through the bucket
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      return bucket[i][1];
+  // check if the current tuple key matches the new key to retrieve
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      // if it does, return the key's value
+        // exit
+      return tuple[1];
     }
   }
+  return null;
 };
 
 HashTable.prototype.remove = function(k) {
+  // get hashed index
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
+  // bucket is the array of tuples at the current index or empty array if index doesn't exist
+  var bucket = this._storage.get(index) || [];
+  // loop through the bucket
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
+  // check if the current tuple key matches the new key to remove
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      // if it does, splice the tuple out of the current bucket
       bucket.splice(i, 1);
+      // decrement
+      this._length--;
+      // check if current size is less than our threshold
+      if (this._length < this._limit * 0.25) {
+        // if it is, call resizer function
+        this._resize(Math.floor(this._limit / 2))
+      }
+      return tuple[1];
     }
   }
-  this._length--;
-  if (this._limit * 0.25 > this._length) {
-    this.resize();
-  }
+  return null;
 };
 
-HashTable.prototype.resize= function() {
-  // inner helper function to resize the hash table and reposition elements
-
-  // if 75% capacity (limit * .75 < length)
-  if (this._limit * 0.75 < this._length) {
-    var copy = this._storage;
-    this._limit *= 2;
-    this._storage = LimitedArray(this._limit);
-    copy.each(function() {
-      return function(bucket, i, storage) {
-
-      var index = getIndexBelowMaxForKey(bucket[0][0], this._limit);
-      // add new hash to hashtable and push temp items into new has
-      this._storage.set(index, bucket);
-    };
-    });
-  } else {
-    // if 25% capacity (limit * .25 > length)
-    var copy = this._storage;
-    this._limit *= 0.5;
-    this._storage = LimitedArray(this._limit);
-    copy.each(function() {
-      return function(bucket, i, storage) {
-      // rehash
-      var index = getIndexBelowMaxForKey(bucket[0][0], this._limit);
-      // add new hash to hashtable and push temp items into new has
-      this._storage.set(index, bucket);
-    };
-    });
-  }
+HashTable.prototype._resize = function(newLimit) {
+  // store the copy of storage into a new variable
+  var copyStorage = this._storage;
+  // set the passed in limit to our this._limit
+  this._limit = newLimit;
+  // set the storage to a new limited array representing the new limit size
+  this._storage = LimitedArray(this._limit);
+  // reset length to 0
+  this._length = 0;
+  // iterate through each of the old storage buckets
+  copyStorage.each(function(bucket) {
+    // if there isn't a bucket
+    if (!bucket) { return; }
+      // return
+    // else insert the current tuple's key and value into the new storage
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
 };
-
-  // var resizer = function(that) {
-  //   console.log(that._storage);
-  //   // loop through all the indexes
-  //   that._storage.each(function(bucket, i, storage) {
-  //       // save the current bucket in temp variable
-  //     var temp = bucket;
-  //     // rehash
-  //     var index = getIndexBelowMaxForKey(bucket[0][0], that._limit);
-  //     // add new hash to hashtable and push temp items into new has
-  //     that._storage.set(index, temp);
-  //   });
-  // };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // Constant time, except for when you resize or when there is a chance that all of the tuples
+ // end up in one bucket
 
 
